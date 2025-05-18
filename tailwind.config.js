e29eca434121912e62bb657fec82ca0efb3777da
sajadanjidani <sajadanjiidanii@gmail.com>
2025-05-18 11:51:46 +0330

    coding : add custome color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
         screens : {
           DEFAULT : '100%'
         }
+      },
+      colors : {
+        textLight : "#000",
+        textDark : "#fff",
+        bgLight: "#ccc",
+        bgDark : "#2a2a2a",
+        darkPrice : "#04ff00",
+        lightPrice : "#008f00",
       }
     },
   },
