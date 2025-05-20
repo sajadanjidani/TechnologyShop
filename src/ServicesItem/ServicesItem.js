1f69b5492c00f38f00bd078898e434adab3dae52
sajadanjidani <sajadanjiidanii@gmail.com>
2025-05-20 23:58:09 +0330

    coding : ServicesItem component

--- src/ServicesItem/ServicesItem.js
+++ src/ServicesItem/ServicesItem.js
@@ -1,14 +1,14 @@
-const ServicesItem = ({children}) => {
+const ServicesItem = ({children, title , content , iconColor , iconBgColor}) => {
     return(
         <div className="flex items-center px-5 w-full h-full rounded-lg bg-white">
             {/* icon container */}
-            <div className="w-14 h-5/6 rounded-full bg-red-600">
-                <div className="flex relative child:absolute child:top-7 child:-left-5">{children}</div>
+            <div id="iconContainer" className="w-14 h-5/6 rounded-full" style={{ backgroundColor: iconBgColor }}>
+                <div className="flex relative child:absolute child:top-7 child:-left-5" style={{ fill: iconColor }}>{children}</div>
             </div>
             {/* content container */}
             <div className="grid py-5 items-center justify-center w-full h-full pr-8">
-                <h2>لورم</h2>
-                <p className="leading-7">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+                <h2>{title}</h2>
+                <p className="leading-7">{content}</p>
             </div>
         </div>
     )
