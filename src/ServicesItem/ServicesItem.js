db593fad6d71a9aa0e895299f89b838a8e5360aa
sajadanjidani <sajadanjiidanii@gmail.com>
2025-05-21 00:02:09 +0330

    create : WeblogContainer component

--- src/ServicesItem/ServicesItem.js
+++ src/ServicesItem/ServicesItem.js
@@ -6,7 +6,7 @@ const ServicesItem = ({children, title , content , iconColor , iconBgColor}) => 
                 <div className="flex relative child:absolute child:top-7 child:-left-5" style={{ fill: iconColor }}>{children}</div>
             </div>
             {/* content container */}
-            <div className="grid py-5 items-center justify-center w-full h-full pr-8">
+            <div className="grid py-9 items-center w-full h-full pr-8">
                 <h2>{title}</h2>
                 <p className="leading-7">{content}</p>
             </div>
