04d2eab12c451f19725c99b3402c50ef1bd273eb
sajadanjidani <sajadanjiidanii@gmail.com>
2025-05-20 23:22:48 +0330

    coding : ServicesWe component

--- src/ServicesWe/ServicesWe.js
+++ src/ServicesWe/ServicesWe.js
@@ -1,9 +1,9 @@
-const ServicesWe = () => {
+const ServicesWe = ({children}) => {
     return(
         <div className="flex flex-col gap-3 w-full h-96 p-5">
             <h1 className="text-xl">خدمات ما</h1>
-            <div className="w-full h-[90%] bg-red-500">
-
+            <div className="grid p-4 gap-4 grid-cols-2 grid-rows-2 w-full h-[90%]">
+                {children}
             </div>
         </div>
     )
