1dc3d99d50ce63276a7b693cc78cecf9820f0420
sajadanjidani <sajadanjiidanii@gmail.com>
2025-05-22 23:47:35 +0330

    coding : responsive Weblog

--- src/WeblogContainer/WeblogContainer.js
+++ src/WeblogContainer/WeblogContainer.js
@@ -2,7 +2,7 @@ const WeblogContainer = ({children}) => {
     return(
         <div className="w-full h-auto p-5 dark:text-textDark dark:child:fill-textDark">
             <h2 className="text-xl">وبلاگ ها</h2>
-            <div className="grid gap-5 grid-cols-4 grid-rows-1 w-full h-96 my-4">
+            <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-1 w-full h-auto my-4 px-5 lg:px-0">
                 {children}
             </div>
         </div>
