f84af6c962a0eb8cb82b6faaff0e84978bbbee0e
sajadanjidani <sajadanjiidanii@gmail.com>
2025-05-23 01:09:11 +0330

    coding : add ul list in navbar menu module

--- src/SliderContainer/SliderContainer.js
+++ src/SliderContainer/SliderContainer.js
@@ -1,6 +1,6 @@
 const SliderContainer = ({children}) => {
     return(
-        <div className="w-[96%] h-72 mt-5 sm:h-[460px] mx-auto">
+        <div className="w-[96%] h-72 mt-28 sm:h-[460px] mx-auto">
             {children}
         </div>
     )
